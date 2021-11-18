05da788e0a075e649243fea1e9a9af25047b5bae
Ilia Dzidzishvili <ilia.dzidzishvili@gmail.com>
2021-11-18 15:52:41 +0400

    some sql data

---
+++ insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name,email,email_verified_at,password,remember_token,created_at,updated_at) VALUES
+('admin','admin@admin.com',NULL,'$2y$10$QwDXJ7aTV1UPWu55smZtDeAlOWxdpAVAJOZaSHvVOSjjjkeEwVHh2',NULL,'2021-11-18 11:46:46.0','2021-11-18 11:46:46.0');
+
+INSERT INTO products (name,price,quantity,created_at,updated_at) VALUES
+('TV',250.55,4,'2021-11-16 17:18:13.0','2021-11-16 17:18:13.0'),
+('Refrigerator',750.00,6,'2021-11-16 17:18:38.0','2021-11-16 17:18:38.0'),
+('PC',450.00,5,'2021-11-16 17:51:11.0','2021-11-16 17:51:11.0'),
+('Washing machine',300.00,5,'2021-11-16 18:21:28.0','2021-11-16 18:21:28.0');
